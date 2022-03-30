14d4ff27e6c577ab08441defa911f82408df311d
MADAMEBlokFluit <102657023+MADAMEBlokFluit@users.noreply.github.com>
2022-03-30 10:44:20 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.showLeds(`
+    # . . . #
+    # # . # #
+    # . # . #
+    # . . . #
+    # . . . #
+    `)
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # # # . .
+    # . . . .
+    # . . . .
+    `)
+basic.forever(function () {
+	
+})
